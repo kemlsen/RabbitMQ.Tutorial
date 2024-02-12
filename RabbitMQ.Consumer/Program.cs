32492a4c7dc52fb21341c2c1fa36e1487661cbb2
Kemal Şen <55662927+kemlsen@users.noreply.github.com>
2024-02-13 00:07:08 +0300

    Add project files.

---
+++ RabbitMQ.Consumer/Program.cs
@@ -0,0 +1,29 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+//Bağlantı Oluşturma
+ConnectionFactory factory = new();
+factory.Uri = new("amqps://jvvojbgk:OhXgCcfKkwDlmcnZPpo_-pbUjKquYAdH@chimpanzee.rmq.cloudamqp.com/jvvojbgk");
+
+//Bağlantıyı Aktifleştirme Ve Kanal Açma
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+//Queue Oluşturma
+channel.QueueDeclare(queue: "example-queue", exclusive: false, durable: true);
+
+//Queue'dan Mesaj Okuma
+EventingBasicConsumer consumer = new(channel);
+channel.BasicConsume(queue: "example-queue", autoAck: false, consumer);
+//autoAck:false -> Normalde kuyruktaki işlem başarılı veya başarısız tüketilmesi halinde kuyruktan işlem silinir. Fakat parametreyi false yaparak işlemin başarılı olması halinde kuyruktaki işlemi sil demiş oluyoruz.
+channel.BasicQos(0, 1, false);
+
+consumer.Received += async (sender, e) =>
+{
+    Console.WriteLine(Encoding.UTF8.GetString(e.Body.Span));
+    channel.BasicAck(deliveryTag: e.DeliveryTag, multiple: false);
+    //basicAck ile işlemin başarılı sonuçlandığını ve multiple:false ile sadece bu işlemin başarılı olduğunu belirtiyoruz
+};
+
+Console.ReadLine();
---
+++ RabbitMQ.Publisher/Program.cs
@@ -0,0 +1,28 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+//Bağlantı Oluşturma
+ConnectionFactory factory = new();
+factory.Uri = new("amqps://jvvojbgk:OhXgCcfKkwDlmcnZPpo_-pbUjKquYAdH@chimpanzee.rmq.cloudamqp.com/jvvojbgk");
+
+//Bağlantıyı Aktifleştirme Ve Kanal Açma
+using IConnection connection = factory.CreateConnection();
+using IModel channel  = connection.CreateModel();
+
+//Queue Oluşturma
+channel.QueueDeclare(queue: "example-queue", exclusive: false, durable: true);
+//exclusive : true -> bu bağlantının dışındaki başka bir bağlantı bu kuyruğu kullanamaz. Bu sebeple false yapıyoruz.
+//durable : true -> Bu durumda kuyruk kalıcı olcak ve rabbitmq restart olsa bile kuyruktaki mesajlar kaybolmayacaktır.
+
+//Queue'ya Mesaj Gönderme
+IBasicProperties properties = channel.CreateBasicProperties();
+properties.Persistent = true;
+
+for (int i = 0; i < 100; i++)
+{
+    await Task.Delay(250);
+    byte[] message = Encoding.UTF8.GetBytes("Merhaba" + i);
+    channel.BasicPublish(exchange: "", routingKey: "example-queue", body: message, basicProperties: properties);
+}
+
+Console.ReadLine();
