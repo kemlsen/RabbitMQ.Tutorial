fda9cc02ccec6d44e21aa999e4004cd61332f40c
Kemal Şen <55662927+kemlsen@users.noreply.github.com>
2024-02-18 16:15:41 +0300

    Added Request Response Pattern In MassTransit

---
+++ RabbitMQ.ESB.MassTransit.RequestResponse.Publisher/Program.cs
@@ -0,0 +1,27 @@
+﻿using MassTransit;
+using RabbitMQ.ESB.MassTransit.Shared.RequestResponseMessage;
+
+Console.WriteLine("Publisher");
+
+string rabbitMQUri = "amqps://jvvojbgk:OhXgCcfKkwDlmcnZPpo_-pbUjKquYAdH@chimpanzee.rmq.cloudamqp.com/jvvojbgk";
+
+string requestQueue = "request-queue";
+
+IBusControl bus = Bus.Factory.CreateUsingRabbitMq(factory =>
+{
+    factory.Host(rabbitMQUri);
+});
+
+await bus.StartAsync();
+
+var request = bus.CreateRequestClient<RequestMessage>(new Uri($"{rabbitMQUri}/{requestQueue}"));
+
+int i = 1;
+while (true)
+{
+    await Task.Delay(200);
+    var response = await request.GetResponse<ResponseMessage>(new() { MessageNo = i, Text = $"{i++}. request" });
+    Console.WriteLine($"Response Received : {response.Message.Text}");
+}
+
+Console.Read();
---
+++ RabbitMQ.ESB.MassTransit.RequestResponsePattern.Consumer/Program.cs
@@ -0,0 +1,22 @@
+﻿using MassTransit;
+using RabbitMQ.ESB.MassTransit.RequestResponsePattern.Consumer.Consumers;
+
+Console.WriteLine("Consumer");
+
+string rabbitMQUri = "amqps://jvvojbgk:OhXgCcfKkwDlmcnZPpo_-pbUjKquYAdH@chimpanzee.rmq.cloudamqp.com/jvvojbgk";
+
+string requestQueue = "request-queue";
+
+IBusControl bus = Bus.Factory.CreateUsingRabbitMq(factory =>
+{
+    factory.Host(rabbitMQUri);
+
+    factory.ReceiveEndpoint(requestQueue, endpoint =>
+    {
+        endpoint.Consumer<RequestMessageConsumer>();
+    });
+});
+
+await bus.StartAsync();
+
+Console.Read();
